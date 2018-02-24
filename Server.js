9714260eef71607e94c866231fa26c21d6ead92f
jcameronjeff <jcameronjeff@gmail.com>
2018-02-24 13:32:18 -0500

    test fix

--- Server.js
+++ Server.js
@@ -17,15 +17,15 @@ var tables = [],
     waitlist = [];
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.get('/tables', (req, res) => {
-    res.sendFile(path.join(__dirname, 'tables.html'));
+    res.sendFile(path.join(__dirname, './tables.html'));
 });
 
 app.get('/reserve', (req, res) => {
-    res.sendFile(path.join(__dirname, 'reserve.html'));
+    res.sendFile(path.join(__dirname, './reserve.html'));
 });
 
 app.get('/api/tables', (req, res) => res.json(tables));
